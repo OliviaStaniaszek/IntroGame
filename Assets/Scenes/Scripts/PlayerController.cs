0f0dc0dcbbc76f37223022cca9ce451f52428a29
r-truesdale <rebecca.truesdale@gmail.com>
2023-10-06 10:41:31 +0100

    scrpt

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
     private int count; //count number of pick-ups collected
+    private int numPickups = 8;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
 
     void Start(){
         count = 0;
+        winText.text = "";
+        SetCountText ();
     }
-    
+
+
     void OnMove(InputValue value)
     {
         moveValue=value.Get<Vector2>();
@@ -30,7 +39,13 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "PickUp"){ //check tag of collided object
             other.gameObject.SetActive(false); // if it was a pickup, deactivate the object
             count += 1;
+            SetCountText();
         }
     }
-
+private void SetCountText(){
+    scoreText.text = "Score: " + count.ToString();
+    if(count >= numPickups){
+        winText.text = "You win!";
+    }
+}
 }
